7af8ffa902fa08901a2d5baebabb87da5afa1c28
ADernild <alex@dernild.dk>
2021-02-08 14:09:28 +0100

    Correcting filedir structure and adding cleaning script

---
+++ scripts/cleaning_speeches.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+# Cleaning function
+clean_speech <- function(x) {
+  x %>%
+    str_replace_all("\\\n", " ") %>% # removes linebreaks
+    str_replace_all("[^[:alnum:]:,.]", " ") %>% # removes special characters except .,
+    str_to_lower() %>% # Converts to lower case
+    str_squish() # Removes leading, trailing and middle whitespace
+}
+
+# Importing data
+df <- read.csv("new_year_speeches_2001-2020.csv")[2:3]
+
+names(df) <- c("speech", "year") # changing first variable name
+
+df$speech <- clean_speech(df$speech)
+
+
+
