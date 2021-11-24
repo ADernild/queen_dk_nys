6f8bf2562b16041fa14d54b344e60785cafa8764
ADernild <alex@dernild.dk>
2021-11-24 12:59:03 +0100

    correcting paths

--- scripts/stm.R
+++ scripts/stm.R
@@ -1,13 +1,16 @@
 library(tidyverse)
 library(stm)
 
-df <- read.csv("nys_2001-2020_cleaned.csv")
-
-# df$x <- clean_speech(df$x)
+df <- read.csv("data/nys_2001-2020_cleaned.csv")
 
 processed <- textProcessor(df$speech, metadata = df, removestopwords = T, language='da')
 
 out <- prepDocuments(processed$documents, processed$vocab, processed$meta)
 
-q_nys <- stm(out$documents, out$vocab, K = 20, prevalence =~ s(year),
+q_nys <- stm(out$documents, out$vocab, K = 0, prevalence =~ s(year),
              max.em.its = 75, data = out$meta, init.type = "Spectral")
+
+plot.STM(q_nys)
+
+labelTopics(q_nys)
+
