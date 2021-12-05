f91db7b9d7c4e3cc26b511360da1f86ae4f2ee07
ADernild <alex@dernild.dk>
2021-12-05 20:38:20 +0100

    commenting out unused packages

--- global.r
+++ global.r
@@ -1,7 +1,7 @@
 library(shiny)
-library(tidyverse) # for code manipulation, data tables, data manipulation, dependencies and a lot more
+library(dplyr) # for code manipulation, data tables, data manipulation, dependencies and a lot more
 library(DT) # For table visualizations
-library(highcharter) # for interactive plots
+# library(highcharter) # for interactive plots
 library(plotly) # for interactive plots
 #library(r2d3) # for advanced interactive plots
 
