81c39c5125ecc7af0a896319a9d1c70b2dfaf133
ADernild <alex@dernild.dk>
2021-12-05 18:22:28 +0100

    modified script to work with new website layout

--- scripts/scraping_speeches.R
+++ scripts/scraping_speeches.R
@@ -1,33 +1,39 @@
 # Web scraping The Queen of Denmark's New Years speeches from kongehuset.dk
 
 # Libraries
-library(tidyverse)
+library(dplyr)
 library(rvest)
 
 # Extracting urls of speeches
-urls <- read_html("https://www.kongehuset.dk/monarkiet-i-danmark/nytarstaler/hendes-majestat-dronningens-nytarstaler") %>%
-  html_nodes(".field-name-field-section-links") %>%
-  html_nodes(".field-items") %>%
+urls <- read_html("https://www.kongehuset.dk/monarkiet-i-danmark/nytarstaler/#laes-de-seneste-nytaarstaler") %>%
+  html_nodes(".accordion__container__item__content") %>% 
+  html_nodes(".field-item") %>% 
   html_nodes("a") %>%
   html_attr("href")
 
+local_urls <- is.na(str_match(urls, "^https://www.kongehuset.dk"))
+
+urls[local_urls] <- lapply(urls[local_urls], function(x) paste0("https://www.kongehuset.dk", x))
+
 # Binding column years to speeches and saving as csv
-speech_year_url <- cbind(year = 2020:2001, urls) %>% 
+last_year <- as.integer(format(Sys.Date(), "%Y")) - 1
+speech_year_url <- cbind(year = last_year:2001, urls) %>% 
   as.data.frame()
 
 # Scraping function
 scrape <- function(link) {
   speech <- read_html(link) %>% 
-  html_nodes(css = '.pad, .field-item.even p') %>% 
+  html_nodes(css = '.rich-text__container__content p') %>% 
     html_text()
   return(speech)
 }
 
 # Scraping speeches
 speeches <- lapply(speech_year_url$urls, scrape) %>% 
-  lapply(paste, collapse = "\n") #collapsing LoL (list of lists)
+  lapply(paste, collapse = " ") #collapsing LoL (list of lists)
 
 # Coercing to data.frame and adding year variable
 df <- do.call(rbind, speeches) %>% 
-  data.frame(year = speech_year_url$year) %>% 
-  write.csv("data/new_year_speeches_2001-2020.csv") #saving as csv
+  cbind(year = last_year:2001) %>% 
+  as.data.frame() %>%  
+  write.csv("data/new_year_speeches_2001-2020.csv", row.names = F) #saving as csv
