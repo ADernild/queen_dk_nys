a1e7408458ee98625c92871ab466913f42dc42bf
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-12-07 15:47:39 +0100

    Added plot with postive and negativte words.

--- ui.r
+++ ui.r
@@ -8,7 +8,16 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins
   radioButtons("modelVis", "Topicmodel", list("STM" = "stm_model", "LDA" = "lda_model")),
   sliderInput("nTerms", "Number of terms to display", min = 10, max = 40, value = 30),
+  
     
   # Show a plot of the generated distribution
-  visOutput("topicmodel")
+  visOutput("topicmodel"),
+  column(width = 12,  
+         column(12,
+                sliderInput("slider_sentiment_of_words_n_words",
+                            "Number of words (by frequency)",
+                            min=1, max=n_dist_t_headword, value = 100 )
+                ),
+         highchartOutput("sentiment_of_words",height="500px")
+  )
 )
