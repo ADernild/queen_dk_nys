255f70296cccb6cdf00bef111b0fdd2945e04eb0
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-12-08 15:08:34 +0100

    Design changes.

--- ui.r
+++ ui.r
@@ -12,17 +12,13 @@ ui <- fluidPage(
                 p("Velkommen til. Dette er et dashboard til at se information om Dronning Margretes nytårstaler. Du kan se information hvilke ord hun siger mest, sentimentet i hendes taler, hvilke lande hun snakker om og hvilke emner hun taler mest om. Du kan sevlfølgelig bruge filtrene til at sætte dine specifikationer til hvad du gerne vil se. God fornøjelse. Gud bevare Danmark.")
               ),
               column(12,
-                     h2("Topic model"),
-                     column(2,
-                            h3("Topic model parameters <3"),
-                              # Sidebar with a slider input for number of bins
-                              radioButtons("topicmodel", "Topicmodel", list("STM" = "stm_model", "LDA" = "lda_model")),
-                              sliderInput("nTerms", "Number of terms to display", min = 10, max = 50, value = 30),
-                            ),
-                     column(10,
-                            h3("Topic model :-O"),
-                              visOutput("topicVis")
-                            )
+                      h2("Topic model"),
+                      h3("Topic model parameters <3"),
+                      # Sidebar with a slider input for number of bins
+                      radioButtons("topicmodel", "Topicmodel", list("STM" = "stm_model", "LDA" = "lda_model")),
+                      sliderInput("nTerms", "Number of terms to display", min = 10, max = 50, value = 30),
+                      h3("Topic model :-O"),
+                      visOutput("topicVis")
                      )
        ),
        column(3,
