8a008b17a43363af01e5aec89fdefc3af14a8ba1
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-12-08 15:49:29 +0100

    Improved formatting from after scraping. Updated results.

--- scripts/scraping_wiki.R
+++ scripts/scraping_wiki.R
@@ -12,9 +12,16 @@ tbls <- html_nodes(webpage, ".infobox")
 queen_info_table <- tbls[1]
 queen_info_table <- as.character(queen_info_table)
 
-# Replace links ----
+# Replace content ----
+## Replace links ----
 queen_info_table <- gsub("/wiki/", "https://da.wikipedia.org/wiki/", queen_info_table)
+queen_info_table <- gsub("#Titler,_pr%C3%A6dikater,_%C3%A6resbevisninger_og_%C3%A6resudn%C3%A6vnelser", "https://da.wikipedia.org/wiki/Margrethe_2.#Titler,_pr%C3%A6dikater,_%C3%A6resbevisninger_og_%C3%A6resudn%C3%A6vnelser", queen_info_table)
+
+## Set links to open in new tab ----
 queen_info_table <- gsub("href", "target='_blank' href", queen_info_table)
+
+## Remove functional text
+queen_info_table <- gsub("Se liste", "", queen_info_table)
 
 # Save as html ----
 fileConn<-file("www/queen_info_table.html", encoding = "UTF-8")
