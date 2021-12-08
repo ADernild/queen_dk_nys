477748d413810bc4f2edb586ec7eac864c11153d
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-12-08 14:44:25 +0100

    Updated structure and added split css into two new css files. Updated design.

--- ui.r
+++ ui.r
@@ -1,31 +1,33 @@
 ui <- fluidPage(
-  # tags$head(
-  #   tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
-  # ),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "royall_beautyfication.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "sizes_n_stuff.css"),
+    tags$title("Queen speech central")
+  ),
   # Application title
-  titlePanel("Queen speech central"),
-  column(12,
-         column(9,
-                div(id = "intoduction",
-                  p("Velkommen til. Dette er et dashboard til at se information om Dronning Margretes nytårstaler. Du kan se information hvilke ord hun siger mest, sentimentet i hendes taler, hvilke lande hun snakker om og hvilke emner hun taler mest om. Du kan sevlfølgelig bruge filtrene til at sætte dine specifikationer til hvad du gerne vil se. God fornøjelse. Gud bevare Danmark.")
-                ),
-                column(12,
-                       h2("topic model"),
-                       column(2,
-                              h3("Topic model parameters <3"),
-                                # Sidebar with a slider input for number of bins
-                                radioButtons("topicmodel", "Topicmodel", list("STM" = "stm_model", "LDA" = "lda_model")),
-                                sliderInput("nTerms", "Number of terms to display", min = 10, max = 50, value = 30),
-                              ),
-                       column(10,
-                              h3("Topic models :-O"),
-                                visOutput("topicVis")
-                              )
-                       )
-         ),
-         column(3,
-                htmlOutput("wiki_infobox")
-         )
+  h1(id="main_title", "Queen speech central"),
+  div(
+       column(9,
+              div(id = "intoduction",
+                p("Velkommen til. Dette er et dashboard til at se information om Dronning Margretes nytårstaler. Du kan se information hvilke ord hun siger mest, sentimentet i hendes taler, hvilke lande hun snakker om og hvilke emner hun taler mest om. Du kan sevlfølgelig bruge filtrene til at sætte dine specifikationer til hvad du gerne vil se. God fornøjelse. Gud bevare Danmark.")
+              ),
+              column(12,
+                     h2("Topic model"),
+                     column(2,
+                            h3("Topic model parameters <3"),
+                              # Sidebar with a slider input for number of bins
+                              radioButtons("topicmodel", "Topicmodel", list("STM" = "stm_model", "LDA" = "lda_model")),
+                              sliderInput("nTerms", "Number of terms to display", min = 10, max = 50, value = 30),
+                            ),
+                     column(10,
+                            h3("Topic model :-O"),
+                              visOutput("topicVis")
+                            )
+                     )
+       ),
+       column(3,
+              htmlOutput("wiki_infobox")
+       )
   ),
   
   # Show a plot of the generated distribution
