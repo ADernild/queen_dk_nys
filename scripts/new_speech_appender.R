70ce00f15f2cf34d5e55d2d1c4298d03c0f763a1
ADernild <alex@dernild.dk>
2021-12-12 01:19:23 +0100

    added a checking statement making sure its the newest speech being appended

--- scripts/new_speech_appender.R
+++ scripts/new_speech_appender.R
@@ -3,20 +3,31 @@ library(dplyr)
 library(rvest)
 
 # Extracting urls of speeches
-urls <- read_html("https://www.kongehuset.dk/monarkiet-i-danmark/nytarstaler/#laes-de-seneste-nytaarstaler") %>%
-  html_nodes(".accordion__container__item__content") %>% 
-  html_nodes(".field-item") %>% 
-  html_nodes("a") %>% 
-  html_attr("href")
+link <- "https://www.kongehuset.dk/monarkiet-i-danmark/nytarstaler/#laes-de-seneste-nytaarstaler"
+last_year <- as.integer(format(Sys.Date(), "%Y")) - 1 # Last year i.e., the year of the lastest speech
+
+url_extractor <- function(link, last_year) {
+  url <- read_html(link) %>%
+    html_nodes(".field-item") %>% 
+    html_nodes("a") %>% 
+    html_attr("href")
+  
+  url_text <- read_html(link) %>%
+    html_nodes(".field-item") %>% 
+    html_nodes("a") %>% 
+    html_text()
+  
+  urls <- tibble(url, url_text)
+  urls <- urls[!is.na(str_extract(urls$url_text, as.character(last_year))),]
+}
+
+urls <- url_extractor(link, last_year)
+urls$url_text <- last_year
 
 # Converting local urls to global urls
-local_urls <- is.na(str_match(urls, "^https://www.kongehuset.dk")) # Checking if url is a local url
-urls[local_urls] <- lapply(urls[local_urls], function(x) paste0("https://www.kongehuset.dk", x)) %>%  # making local urls global
+local_urls <- is.na(str_match(urls$url, "^https://www.kongehuset.dk")) # Checking if url is a local url
+urls$url[local_urls] <- lapply(urls$url[local_urls], function(x) paste0("https://www.kongehuset.dk", x)) %>%  # making local urls global
   unlist()
-
-# Binding column urls to years kongehuset
-last_year <- as.integer(format(Sys.Date(), "%Y")) - 1 # Last year i.e., the year of the lastest speech
-kongehuset <- data.frame(year = last_year:2001, urls)
 
 # Scraping functions
 scrape_kongehuset <- function(url) {
@@ -27,7 +38,7 @@ scrape_kongehuset <- function(url) {
 }
 
 # Scraping newest speech
-new_speech <- scrape_kongehuset(kongehuset$urls[1]) %>% 
+new_speech <- scrape_kongehuset(urls$url) %>% 
   paste(collapse = " ")
 
 new_speech <- data.frame(speech = new_speech, year = last_year)
