9e8f4310b3e570e1978246c30589bff27f948c29
ADernild <alex@dernild.dk>
2022-03-26 22:07:03 +0100

    ran stm on news articles

--- scripts/stm.R
+++ scripts/stm.R
@@ -8,6 +8,7 @@ library(SnowballC)
 
 # Reading data
 df <- read.csv("data/sentences.csv", encoding = "UTF-8")
+# df <- readRDS("data/sentences.rds")
 
 # Making stopwords list
 stop_words <- read.csv("utils/stopord.txt", header=F) %>% 
@@ -19,33 +20,34 @@ stop_words <- read.csv("utils/stopord.txt", header=F) %>%
 # STM 
 make_stm_model <- function(docs, df, covariates, stop_words, language="danish") {
   # Preprocessing documents for use in STM
-  processed <- textProcessor(docs, metadata=df, stem=T, customstopwords=stop_words, removenumbers = F, language=language)
+  processed <- textProcessor(docs, metadata=df, stem=T, customstopwords=stop_words, removenumbers = T, language=language)
   out <- prepDocuments(processed$documents, processed$vocab, processed$meta)
   
-  # finding best amount of topics K
-  best_model <- searchK(out$documents, out$vocab, K=c(10:35),
-                        init.type="Spectral", proportion=0.1,
-                        heldout.seed=1337, prevalence=covariates,
-                        data=out$meta)
-
+  # # finding best amount of topics K
+  # best_model <- searchK(out$documents, out$vocab, K=c(15,30,45,60,75,90,105),
+  #                       init.type="Spectral", proportion=0.1,
+  #                       heldout.seed=1337, prevalence=covariates,
+  #                       data=out$meta,
+  #                       cores=parallel::detectCores())
+  # 
+  best_model <- readRDS("data/best_model.rds") # Generated before model creation
   best_k <- best_model$results$K[which.max(best_model$results$heldout)][[1]]
-
-  # Training model
-  model <- stm(out$documents, out$vocab, K=best_k, prevalence=covariates,
-               max.em.its=300, data=out$meta, init.type="Spectral", verbose=F)
+# Training model
+  model <- stm(out$documents, out$vocab, K=100, prevalence=covariates,
+             max.em.its=300, data=out$meta, init.type="Spectral", verbose=T)
 
   thoughts <- findThoughts(model, out$meta$sentences_full, n=200, thresh = 0.45)
-  
+
   thoughts$years <- lapply(thoughts$index, function(i) out$meta$years[i])
   thoughts$polarity <- lapply(thoughts$index, function(i) out$meta$polarity[i])
-  
-  # Saving model results in list
-  stm_model <- list(mod=model,
-                    docs=out$documents,
-                    best=best_model)
 
-  
-  list(stm_model = stm_model, thoughts = thoughts)
+# Saving model results in list
+stm_model <- list(mod=model,
+                  docs=out$documents,
+                  best=best_model)
+
+
+list(stm_model = stm_model, thoughts = thoughts)
 }
 
 stm_da <- make_stm_model(df$sentences, df, covariates=formula(~uuid + polarity), stop_words$word)
