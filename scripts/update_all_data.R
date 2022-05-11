5f2f22ae31b8c6f9d828c2ca9656119fe8046be1
ADernild <alex@dernild.dk>
2022-05-11 17:26:19 +0200

    added a working directory (needed to make it work with docker)

--- scripts/update_all_data.R
+++ scripts/update_all_data.R
@@ -1,6 +1,7 @@
 # Update  all data ----------------------------------------------------------
 # Scrapes content, updates libarary, creates tokens and token sentiment,
 # sentiment for sentences and articles, and runs stm analysis
+setwd("/usr/local/src/") # location for scripts, data etc. in docker container
 source("scripts/detach_packages.R")
 source("scripts/scraping.R")
 source("scripts/cleaning.R")
