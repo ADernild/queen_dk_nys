6624a51614ab9e62cb26471bbe5246a90c4fb3df
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2022-06-14 14:51:42 +0200

    Removed unused code.

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -37,10 +37,7 @@ if(nrow(tokens)>0){
   
   # Stemming ----
   tokens$stemmed <- wordStem(tokens$word, language = "danish") # stemming
-  # tokens$stemmed_hunspell <- hunspell::hunspell_stem(tokens$word, dict = dictionary('da_DK')) # Dictionary based stemming
-  # hunspell::hunspell_stem(tokens$word, dict = dictionary('da_DK'))
-  
-  
+
   # All tokens, filtered
   # tokens <- readRDS("data/tokens.rds")
   tokens <- tokens %>% 
@@ -49,41 +46,7 @@ if(nrow(tokens)>0){
            date_updated_at = article_lib$date_updated_at[min(which(uuid == article_lib$uuid))],
            date_published_at = article_lib$date_published_at[min(which(uuid == article_lib$uuid))]
     )
-  
-  # # Lemmatization ----
-  # model <- udpipe_download_model(language = "danish")
-  # if(!model$download_failed){
-  #   ud <- udpipe_load_model(model)
-  #   
-  #   ## Tokenise, Tag and Dependency Parsing Annotation. Output is in CONLL-U format.
-  #   txt <- df$content
-  #   # txt <- paste(df$content, collapse = ' ')
-  #   names(txt) <- df$uuid
-  #   lemmi <- udpipe(txt, object = ud)
-  #   lemmi <- udpipe(data.frame(doc_id = names(txt), text = txt, stringsAsFactors = FALSE), 
-  #               object = ud)
-  #   lemmi <- udpipe(strsplit(txt, "[[:space:][:punct:][:digit:]]+"), 
-  #               object = ud)
-  #   lemmi <- lemmi[colSums(!is.na(lemmi)) > 0]
-  # }
-  # 
-  # saveRDS(lemmi, "data/lemma.rds")
-  # lemma <- readRDS("data/lemma.rds")
-  # 
-  # ## Implement legitimized references into tokens ----
-  # tokens <- tokens %>%
-  #   rowwise() %>% 
-  #   mutate(lemma = paste(unique(lemmi[lemmi$token == word,]$lemma), collapse=","))
-  # 
-  # ## Count total occurrences of limmitized words ----
-  # total_tokens <- tokens %>%
-  #   group_by(lemma) %>%
-  #   summarise(n_lemma_total = sum(n_in))
-  # 
-  # tokens <- tokens %>%
-  #   left_join(total_tokens, by="lemma") %>% 
-  #   arrange(desc(n_lemma_total), word, desc(n_stem_total), desc(n_total)) # arrange
-  
+ 
   # Combine token lists ----
   if(exists("old_tokens")){
     tokens <- tokens %>% 
